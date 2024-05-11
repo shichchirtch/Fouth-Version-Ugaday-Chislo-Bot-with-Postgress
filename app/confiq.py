83c1818eaa2b12805adcd2edb2eda849a6f87f8b
shichchirtch <ivanvol9l@gmail.com>
2024-05-11 03:24:12 +0200

    2 Commit config fix

--- app/confiq.py
+++ app/confiq.py
@@ -1,7 +1,7 @@
 
-host ='172.28.221.122'
+host = '172.28.221.122'
 user = 'postgres'
 password = 'postgres'
-db_name = 'postgres'
+db_name = 'bot_local_base'
 
 db_path = f"postgresql+asyncpg://{user}:{password}@{host}:5432/{db_name}"
